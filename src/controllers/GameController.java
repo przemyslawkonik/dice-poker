469c898b54d12b06b363c1bcfb281ee68f7957cd
Przemysław Konik <przemyslawkonik@gmail.com>
2017-06-05 21:03:35 +0200

    progress - added game controller

---
+++ src/controllers/GameController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Przemysław Konik on 2017-06-05.
+ */
+public class GameController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    
+}
