2a5e4e3db20eeb2731e86ec77a6f3d140cdc39ca
Przemysław Konik <przemyslawkonik@gmail.com>
2017-06-05 15:58:01 +0200

    pom updated, main class created

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Przemysław Konik on 2017-06-05.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
