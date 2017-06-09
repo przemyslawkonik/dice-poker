c4143940175896ba738761d3611269c8fa7e22d2
Przemysław Konik <przemyslawkonik@gmail.com>
2017-06-09 14:36:08 +0200

    progress - clean some code

--- src/controllers/InfoBoxController.java
+++ src/controllers/InfoBoxController.java
@@ -2,25 +2,17 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 /**
  * Created by Przemysław Konik on 2017-06-08.
  */
-public class InfoBoxController implements Initializable {
+public class InfoBoxController {
 
     @FXML
     private Label label;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    }
 
     public void handleButton(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
