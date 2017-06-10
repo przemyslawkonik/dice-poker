3cda6da39a12c82ab5fedee2033fdf0f411a506c
Przemysław Konik <przemyslawkonik@gmail.com>
2017-06-10 12:28:26 +0200

    progress - added main window title

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainView.fxml"));
         primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.setTitle("Dice poker");
         primaryStage.show();
     }
 
